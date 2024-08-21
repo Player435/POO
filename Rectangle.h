0daf221035a240f6989f3d3d33b27a031a22365c
Player435 <walksrajo@gmail.com>
2024-08-21 17:45:30 +0300

    Create Rectangle.h

---
+++ Rectangle.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Rectangle
+{
+private:
+    double longueur;
+    double largeur;
+
+public:
+    Rectangle() ; 
+    Rectangle(double L, double l) : longueur(L), largeur(l);
+};
+    
